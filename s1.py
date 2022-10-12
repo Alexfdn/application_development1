f0258f0b746a73e2979703395e77ded3535be3c2
Alexfdn <115472815+Alexfdn@users.noreply.github.com>
2022-10-12 13:15:12 +0500

    Add files via upload

---
+++ s1.py
@@ -0,0 +1,14 @@
+a = int(input())
+for i in range (2, a+1):
+    if a % i == 0:
+        break
+print(i)
+b = int(input())
+for i in range (1, b + 1):
+    if 5 <= i <= 9:
+        continue
+    if 17 <= i <= 37:
+        continue
+    if 78 <= i <= 87:
+        continue
+    print(i)
